040cf01046db5a7e66f4e7ea03d5949742cce8b3
Alper Çugun <git@alper.nl>
2012-05-02 20:46:39 +0200

    Initial Commit

---
+++ Transiter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Transiter
+//
+//  Created by Alper Çuğun on 2/5/12.
+//  Copyright (c) 2012 Aardverschuiving Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Transiter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Transiter
+//
+//  Created by Alper Çuğun on 2/5/12.
+//  Copyright (c) 2012 Aardverschuiving Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
